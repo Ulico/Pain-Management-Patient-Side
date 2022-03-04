4c2bc93d914b66fb408737b70801b0c6dc62578c
Adrian <aruss8@illinois.edu>
2022-03-04 14:43:49 -0600

    added functionality to EmotionRecordActivity

--- app/src/main/java/com/adrianrusso/painmanagementpatientside/activites/EmotionRecordActivity.kt
+++ app/src/main/java/com/adrianrusso/painmanagementpatientside/activites/EmotionRecordActivity.kt
@@ -4,12 +4,14 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import androidx.annotation.RequiresApi
 import com.adrianrusso.painmanagementpatientside.databinding.ActivityCheckinBinding
 import com.adrianrusso.painmanagementpatientside.databinding.ActivityEmotionRecordBinding
 import com.adrianrusso.painmanagementpatientside.models.AppUser
 import com.adrianrusso.painmanagementpatientside.models.Submission
 import com.google.android.material.slider.Slider
+import com.google.android.material.snackbar.Snackbar
 
 
 class EmotionRecordActivity : AppCompatActivity() {
@@ -26,13 +28,22 @@ class EmotionRecordActivity : AppCompatActivity() {
                 binding.emotionRatingText.text = it
             }
         })
+
+        binding.treatmentsSpinner.adapter =
+            ArrayAdapter(this, android.R.layout.simple_spinner_item, AppUser.commonTreatments)
+        binding.activitesSpinner.adapter =
+            ArrayAdapter(this, android.R.layout.simple_spinner_item, AppUser.activities)
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
     fun onSubmit(view: View) {
         val s = Submission()
         s.feelLevel = binding.feelSlider.value.toInt()
+        s.treatments = listOf(binding.treatmentsSpinner.selectedItem.toString())
+        s.activity = binding.activitesSpinner.selectedItem.toString()
         AppUser.sendSubmission(s)
+
+        Snackbar.make(view, "Emotion level submitted", Snackbar.LENGTH_SHORT).show()
 
         finish()
     }
