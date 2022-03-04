6c9dbb10b61f73622b6546215dc8b08ea5e63157
Adrian <aruss8@illinois.edu>
2022-03-04 00:57:03 -0600

    create ViewPager

--- app/src/main/java/com/adrianrusso/painmanagementpatientside/activites/MedicationRecordActivity.kt
+++ app/src/main/java/com/adrianrusso/painmanagementpatientside/activites/MedicationRecordActivity.kt
@@ -5,14 +5,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.adrianrusso.painmanagementpatientside.R
+import com.adrianrusso.painmanagementpatientside.adapters.MedicationPagerAdapter
+import com.adrianrusso.painmanagementpatientside.databinding.ActivityMainBinding
+import com.adrianrusso.painmanagementpatientside.databinding.ActivityMedicationRecordBinding
 
 class MedicationRecordActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMedicationRecordBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_medication_record)
+        binding = ActivityMedicationRecordBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.horizontalScrollView.adapter = MedicationPagerAdapter(this)
     }
 
     fun onSubmit(view: View) {
         startActivity(Intent(this, CheckinActivity::class.java))
+    }
+
+    fun onAddNew(view: View) {
+        startActivity(Intent(this, AddMedicationActivity::class.java))
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.horizontalScrollView.adapter?.notifyDataSetChanged()
     }
 }
---
+++ app/src/main/java/com/adrianrusso/painmanagementpatientside/adapters/MedicationPagerAdapter.kt
@@ -0,0 +1,45 @@
+package com.adrianrusso.painmanagementpatientside.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.viewpager.widget.PagerAdapter
+import com.adrianrusso.painmanagementpatientside.R
+import com.adrianrusso.painmanagementpatientside.models.AppUser
+import com.adrianrusso.painmanagementpatientside.models.Medication
+
+
+class MedicationPagerAdapter(private val mContext: Context) : PagerAdapter() {
+
+
+    override fun instantiateItem(collection: ViewGroup, position: Int): Any {
+        val medication: Medication = AppUser.medications[position]
+        val inflater = LayoutInflater.from(mContext)
+        val view = inflater.inflate(R.layout.pager_item_medication, collection, false)
+        medication.purpose.also { (view.findViewById(R.id.purposeText) as TextView).text = it }
+        "Medicine: ${medication.name}".also {
+            (view.findViewById(R.id.nameText) as TextView).text = it
+        }
+        "Dose: ${medication.dose}".also { (view.findViewById(R.id.doseText) as TextView).text = it }
+        "Instruction: ${medication.instruction}".also {
+            (view.findViewById(R.id.instructionText) as TextView).text = it
+        }
+        collection.addView(view)
+        return view
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as View?)
+    }
+
+
+    override fun getCount(): Int {
+        return AppUser.medications.size
+    }
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view === `object`
+    }
+}
