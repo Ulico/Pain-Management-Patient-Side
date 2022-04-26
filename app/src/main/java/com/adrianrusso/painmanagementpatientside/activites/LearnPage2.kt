4ae76b831a815d89094296ee03c136156062b5e7
Adrian <aruss8@illinois.edu>
2022-04-26 18:52:34 -0500

    added education and gaming

---
+++ app/src/main/java/com/adrianrusso/painmanagementpatientside/activites/LearnPage2.kt
@@ -0,0 +1,12 @@
+package com.adrianrusso.painmanagementpatientside.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.adrianrusso.painmanagementpatientside.R
+
+class LearnPage2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_learn_page2)
+    }
+}
--- app/src/main/java/com/adrianrusso/painmanagementpatientside/fragments/EducationFragment.kt
+++ app/src/main/java/com/adrianrusso/painmanagementpatientside/fragments/EducationFragment.kt
@@ -1,10 +1,15 @@
 package com.adrianrusso.painmanagementpatientside.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.adrianrusso.painmanagementpatientside.activites.LearnPage1
+import com.adrianrusso.painmanagementpatientside.activites.LearnPage2
+import com.adrianrusso.painmanagementpatientside.activites.LearnPage3
+import com.adrianrusso.painmanagementpatientside.activites.LearnPage4
 import com.adrianrusso.painmanagementpatientside.databinding.FragmentEducationBinding
 
 class EducationFragment : Fragment() {
@@ -13,6 +18,13 @@ class EducationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val binding = FragmentEducationBinding.inflate(inflater, container, false)
+
+        binding.button1.setOnClickListener { startActivity(Intent(activity, LearnPage1::class.java)) }
+        binding.button2.setOnClickListener { startActivity(Intent(activity, LearnPage2::class.java)) }
+        binding.button3.setOnClickListener { startActivity(Intent(activity, LearnPage3::class.java)) }
+        binding.button4.setOnClickListener { startActivity(Intent(activity, LearnPage4::class.java)) }
+
+
         return binding.root
     }
 }
